7c09f5b6f11c4d8ecf8b49611bfbba1f583436aa
erikvw <ew2789@gmail.com>
2022-04-25 08:29:32 -0500

    build, tox

--- effect_form_validators/__init__.py
+++ effect_form_validators/__init__.py
@@ -1,7 +1,8 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
+# from importlib.metadata import PackageNotFoundError, version
+#
+# try:
+#     __version__ = version(__name__)
+# except PackageNotFoundError:
+#     # package is not installed
+#     pass
+from ._version import version as __version__
