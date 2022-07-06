bdaaf24cece26ef25d228fd858b29918877022ca
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-07-06 16:41:00 +0100

    Test all scheduled post baseline visits (and unscheduled d1 visit)

--- effect_form_validators/tests/effect_subject/test_vital_signs.py
+++ effect_form_validators/tests/effect_subject/test_vital_signs.py
@@ -25,16 +25,29 @@ class TestVitalSignsFormValidator(TestCaseMixin, TestCase):
         self.addCleanup(patcher.stop)
         self.mock_is_baseline = patcher.start()
 
-    def get_cleaned_data(self, visit_code: Optional[str] = None, **kwargs) -> dict:
-        cleaned_data = super().get_cleaned_data(visit_code=visit_code, **kwargs)
+    def get_cleaned_data(
+        self,
+        visit_code: Optional[str] = None,
+        visit_code_sequence: Optional[int] = None,
+        **kwargs,
+    ) -> dict:
+        cleaned_data = super().get_cleaned_data(
+            visit_code=visit_code,
+            visit_code_sequence=visit_code_sequence,
+            **kwargs,
+        )
+        baseline = (
+            cleaned_data.get("subject_visit").visit_code == DAY01
+            and cleaned_data.get("subject_visit").visit_code_sequence == 0
+        )
         cleaned_data.update(
             weight=60.0,
             weight_measured_or_est="measured",
             heart_rate=60,
             respiratory_rate=14,
             temperature=37.0,
-            reportable_as_ae=NOT_APPLICABLE if visit_code == DAY01 else NO,
-            patient_admitted=NOT_APPLICABLE if visit_code == DAY01 else NO,
+            reportable_as_ae=NOT_APPLICABLE if baseline else NO,
+            patient_admitted=NOT_APPLICABLE if baseline else NO,
         )
         return cleaned_data
 
@@ -47,16 +60,19 @@ class TestVitalSignsFormValidator(TestCaseMixin, TestCase):
         except ValidationError as e:
             self.fail(f"ValidationError unexpectedly raised. Got {e}")
 
-    def test_cleaned_data_at_d14_ok(self):
+    def test_cleaned_data_after_baseline_ok(self):
         self.mock_is_baseline.return_value = False
-        cleaned_data = self.get_cleaned_data(
-            report_datetime=self.consent_datetime + relativedelta(days=14),
-        )
-        form_validator = VitalSignsFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f"ValidationError unexpectedly raised. Got {e}")
+        for visit_code in self.visit_schedule:
+            with self.subTest(visit_code=visit_code):
+                cleaned_data = self.get_cleaned_data(
+                    visit_code=visit_code,
+                    visit_code_sequence=1 if visit_code == DAY01 else 0,
+                )
+                form_validator = VitalSignsFormValidator(cleaned_data=cleaned_data)
+                try:
+                    form_validator.validate()
+                except ValidationError as e:
+                    self.fail(f"ValidationError unexpectedly raised. Got {e}")
 
     def test_reportable_as_ae_not_applicable_at_baseline(self):
         self.mock_is_baseline.return_value = True
