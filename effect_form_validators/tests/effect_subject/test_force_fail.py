4f4a5fac06775c5668ec10b4730a86ea8cc86acc
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-11-14 17:16:35 +0000

    Remove deliberately failing test from develop

--- effect_form_validators/tests/effect_subject/test_force_fail.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-
-class TestForcedFail(TestCase):
-    def test_deliberate_fail(self):
-        self.fail("Deliberate failing test to validate GH actions badge behaviour")
